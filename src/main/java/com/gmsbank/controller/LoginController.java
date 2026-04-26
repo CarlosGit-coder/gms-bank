30c571b5481a4dfbd003b24a233138c27f796b42
CarlosGit-coder <carlosgabrielmonteiro2006@gmail.com>
2026-04-26 19:38:05 -0300

    Criei o metodo login e o controller do dashboard

--- src/main/java/com/gmsbank/controller/LoginController.java
+++ src/main/java/com/gmsbank/controller/LoginController.java
@@ -1,7 +1,13 @@
 package com.gmsbank.controller;
 
+import com.gmsbank.model.Usuarios;
+import com.gmsbank.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
@@ -9,6 +15,20 @@ public class LoginController {
     @GetMapping("/login")
     public String roteLogin(){
         return "login";
+    }
+
+    @Autowired
+    private AuthService authService;
+
+    @PostMapping("/login")
+    public String login(@RequestParam String  email, @RequestParam String senha, RedirectAttributes redirectAttributes) {
+        Usuarios usuario = authService.autenticar(email, senha);
+        if (usuario == null) {
+            redirectAttributes.addFlashAttribute("erro", "login invalido");
+            return "redirect:/login";
+        } else {
+            return "redirect:/dashboard";
+        }
     }
 
 
