e70cb932c33571be7e059604ec5bd75f2b094fc1
CarlosGit-coder <carlosgabrielmonteiro2006@gmail.com>
2026-04-27 13:49:08 -0300

    a criação daa busca, deleta e edição dos dados dos clientes, criação de um buscador de clientes

--- src/main/java/com/gmsbank/controller/ClienteController.java
+++ src/main/java/com/gmsbank/controller/ClienteController.java
@@ -1,10 +1,16 @@
 package com.gmsbank.controller;
 
+import com.gmsbank.model.Clientes;
 import com.gmsbank.repository.ClientesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class ClienteController {
@@ -19,6 +25,43 @@ public class ClienteController {
         model.addAttribute("todosClientes", todosClientes);
 
         return "clientes";
+    }
+
+    @PostMapping("/clientes/cadastrar")
+    public String cadastrarClientes(@ModelAttribute Clientes clientes) {
+        clientesRepository.save(clientes);
+        return "redirect:/clientes";
+    }
+
+    @GetMapping("/clientes/editar/{id}")
+    public String editarClientes(@PathVariable Long id, Model model) {
+        Clientes clientes = clientesRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID não localizado"));
+        model.addAttribute("clientes", clientes);
+        return "editarClientes";
+    }
+
+    @PostMapping("/clientes/editar/{id}")
+    public String salvarEdicao(@PathVariable Long id, @ModelAttribute Clientes clientes) {
+        clientes.setPK_id_clientes(id);
+        clientesRepository.save(clientes);
+        return "redirect:/clientes";
+    }
+
+    @PostMapping("/clientes/deletar/{id}")
+    public String deletarCliente(@PathVariable Long id) {
+        clientesRepository.deleteById(id);
+        return "redirect:/clientes";
+    }
+
+    @GetMapping("/clientes/contas")
+    public String clientesComcontas(Model model) {
+
+        List<Object[]> dados = clientesRepository.buscarClientesComContas();
+
+        model.addAttribute("dados", dados);
+
+        return "clientescontas";
     }
 
 }
--- src/main/java/com/gmsbank/repository/ClientesRepository.java
+++ src/main/java/com/gmsbank/repository/ClientesRepository.java
@@ -2,8 +2,18 @@ package com.gmsbank.repository;
 
 import com.gmsbank.model.Clientes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ClientesRepository extends JpaRepository<Clientes, Integer> {
+
+@Query(value = """
+ SELECT c.nome_clientes, co.numero_contas, co.saldo_contas
+    FROM clientes c
+    INNER JOIN contas co ON c.PK_id_clientes = co.FK_id_clientes
+    """, nativeQuery = true)
+    List<Object[]> buscarClientesComContas();
 }
